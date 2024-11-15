fa8cb688730d5a359c866131601b0a5d111330d5
CoderScholar64 <126514127+CoderScholar64@users.noreply.github.com>
2024-11-15 00:39:28 -0800

    Already picking up an error

---
+++ src-tests/unicode.c
@@ -0,0 +1,35 @@
+#include <stddef.h>
+#include <stdint.h>
+
+#define CS64_INI_LIBRARY_IMP
+#include "cs64_ini.h"
+
+#include <stdio.h>
+
+int main() {
+    CS64UTF8 utf8_data[8];
+    CS64Size characterByteSize = 1;
+
+    // Reencoding Test.
+    CS64UniChar c = 0;
+    while(c < CS64_INI_MAX_CODE + 1) {
+        int writeResult = cs64_ini_utf_8_write(utf8_data, sizeof(utf8_data) / sizeof(utf8_data[0]), c);
+
+        if(writeResult <= 0) {
+            printf("cs64_ini_utf_8_write failed for unicode char %i with error code %i\n", c, writeResult);
+
+            return 1;
+        }
+
+        CS64UniChar character = cs64_ini_utf_8_read(utf8_data, writeResult, &characterByteSize);
+
+        if(character != c) {
+            printf("cs64_ini_utf_8_read failed for unicode char %i produced %i\n", c, character);
+
+            return 2;
+        }
+        c++;
+    }
+
+    return 0;
+}
