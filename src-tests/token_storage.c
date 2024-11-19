aca4657793fca9f9c53a7d5890e973bde37a8f97
CoderScholar64 <126514127+CoderScholar64@users.noreply.github.com>
2024-11-19 14:33:13 -0800

    Empty case added

--- src-tests/token_storage.c
+++ src-tests/token_storage.c
@@ -7,6 +7,9 @@ void test_free(void *pointer);
 #define CS64_INI_MALLOC(size)  test_malloc(size)
 #define CS64_INI_FREE(pointer) test_free(pointer)
 
+// This limit would make this test easier to write.
+#define CS64_INI_TOKEN_AMOUNT 4
+
 #define CS64_INI_LIBRARY_IMP
 #include "cs64_ini.h"
 
@@ -19,19 +22,30 @@ void *pPointerTrackArray[TACKER_ARRAY_LIMIT];
 unsigned pointerTrackAmount = 0;
 
 int main() {
+    CS64INITokenData *pTokenData = cs64_ini_token_data_alloc();
+
+    if(pTokenData == NULL) {
+        printf("Error: pTokenData failed to allocate!\n");
+        return 1;
+    }
+
+    cs64_ini_token_data_free(pTokenData);
+
     return 0;
 }
 
 void *test_malloc(size_t size) {
+    printf("Log: Allocating pointer of size 0x%x", size);
     void *pointer = malloc(size);
+    printf(". Address %p\n", pointer);
 
     if(pointer == NULL) {
-        printf("Error: Pointer for size 0x%x cannot be allocated!", size);
+        printf("Error: Pointer for size 0x%x cannot be allocated!\n", size);
         exit(EXIT_FAILURE); // Out of memory
     }
 
     if(pointerTrackAmount == TACKER_ARRAY_LIMIT) {
-        printf("Error: Traker Limit reached %i", TACKER_ARRAY_LIMIT);
+        printf("Error: Traker limit reached %i\n", TACKER_ARRAY_LIMIT);
         exit(EXIT_FAILURE); // Tracker ran out of memory.
     }
 
@@ -42,19 +56,20 @@ void *test_malloc(size_t size) {
 }
 
 void test_free(void *pPointer) {
+    printf("Log: Freeing %p\n", pPointer);
     unsigned pointerTrackIndex = 0;
-    while(pointerTrackIndex < pointerTrackAmount && pPointerTrackArray[pointerTrackIndex] == pPointer) {
+    while(pointerTrackIndex < pointerTrackAmount && pPointerTrackArray[pointerTrackIndex] != pPointer) {
         pointerTrackIndex++;
     }
 
     if(pointerTrackIndex == pointerTrackAmount) {
-        printf("Error: Failed to locate %p within the track array. Probably a double free.", pPointer);
+        printf("Error: Failed to locate %p within the track array. Probably a double free or a pointer not allocated by test_malloc\n", pPointer);
         exit(EXIT_FAILURE);
     }
 
     free(pPointer);
 
-    // Remove it from the track array.
+    // Remove the pointer from the track array.
     pPointerTrackArray[pointerTrackIndex] = pPointerTrackArray[pointerTrackAmount - 1];
     pointerTrackAmount--;
 }
