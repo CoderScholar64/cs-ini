3833d35d5721f2ae6dde9ffba633786e0163521d
CoderScholar64 <126514127+CoderScholar64@users.noreply.github.com>
2024-11-19 14:05:28 -0800

    Testing functions implemented.

--- src-tests/token_storage.c
+++ src-tests/token_storage.c
@@ -1,9 +1,60 @@
 #include <stddef.h>
 #include <stdint.h>
 
+void *test_malloc(size_t size);
+void test_free(void *pointer);
+
+#define CS64_INI_MALLOC(size)  test_malloc(size)
+#define CS64_INI_FREE(pointer) test_free(pointer)
+
 #define CS64_INI_LIBRARY_IMP
 #include "cs64_ini.h"
 
+#include <stdlib.h>
+#include <stdio.h>
+
+#define TACKER_ARRAY_LIMIT 64
+
+void *pPointerTrackArray[TACKER_ARRAY_LIMIT];
+unsigned pointerTrackAmount = 0;
+
 int main() {
     return 0;
+}
+
+void *test_malloc(size_t size) {
+    void *pointer = malloc(size);
+
+    if(pointer == NULL) {
+        printf("Error: Pointer for size 0x%x cannot be allocated!", size);
+        exit(EXIT_FAILURE); // Out of memory
+    }
+
+    if(pointerTrackAmount == TACKER_ARRAY_LIMIT) {
+        printf("Error: Traker Limit reached %i", TACKER_ARRAY_LIMIT);
+        exit(EXIT_FAILURE); // Tracker ran out of memory.
+    }
+
+    pPointerTrackArray[pointerTrackAmount] = pointer;
+    pointerTrackAmount++;
+
+    return pointer;
+}
+
+void test_free(void *pPointer) {
+    unsigned pointerTrackIndex = 0;
+    while(pointerTrackIndex < pointerTrackAmount && pPointerTrackArray[pointerTrackIndex] == pPointer) {
+        pointerTrackIndex++;
+    }
+
+    if(pointerTrackIndex == pointerTrackAmount) {
+        printf("Error: Failed to locate %p within the track array. Probably a double free.", pPointer);
+        exit(EXIT_FAILURE);
+    }
+
+    free(pPointer);
+
+    // Remove it from the track array.
+    pPointerTrackArray[pointerTrackIndex] = pPointerTrackArray[pointerTrackAmount - 1];
+    pointerTrackAmount--;
 }
