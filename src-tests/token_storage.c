5f49a0582894e69c4eb22da09f91b7f5e79d4a8a
CoderScholar64 <126514127+CoderScholar64@users.noreply.github.com>
2024-11-19 17:19:30 -0800

    Merged two tests together. Added struct tests

--- src-tests/token_storage.c
+++ src-tests/token_storage.c
@@ -22,56 +22,58 @@ void *pPointerTrackArray[TACKER_ARRAY_LIMIT];
 unsigned pointerTrackAmount = 0;
 int disableTestMalloc = 0;
 
-int valid_alloc_test();
+int empty_alloc_test();
 int invalid_alloc_test();
 
 int main() {
     int testResult;
 
-    testResult = valid_alloc_test();
-    if(testResult != 0)
-        return testResult;
-
-    testResult = invalid_alloc_test();
+    testResult = empty_alloc_test();
     if(testResult != 0)
         return testResult;
 
     return testResult;
 }
 
-int valid_alloc_test() {
-    disableTestMalloc = 0;
-
-    CS64INITokenData *pTokenData = cs64_ini_token_data_alloc();
-
-    if(pTokenData == NULL) {
-        printf("Error: pTokenData failed to allocatefor valid_alloc_test case! There is a very slight chance that the program ran out of memory.\n");
-        return 1;
-    }
-
-    cs64_ini_token_data_free(pTokenData);
-
-    if(pointerTrackAmount != 0) {
-        printf("Error: pointerTrackAmount is supposed to be zero after test not be %i.\n", pointerTrackAmount);
-        return 2;
-    }
-
-    return 0;
-}
-
-int invalid_alloc_test() {
+int empty_alloc_test() {
     disableTestMalloc = 1;
 
     CS64INITokenData *pTokenData = cs64_ini_token_data_alloc();
 
     if(pTokenData != NULL) {
-        printf("Error: cs64_ini_token_data_alloc was supposed to fail for invalid_alloc_test case. Not return %p.\n", pTokenData);
+        printf("Error: cs64_ini_token_data_alloc was supposed to fail for empty_alloc_test case. Not return %p.\n", pTokenData);
         return 1;
     }
 
-    if(pointerTrackAmount != 0) {
-        printf("Error: pointerTrackAmount is supposed to be zero after test not be %i.\n", pointerTrackAmount);
+    disableTestMalloc = 0;
+
+    pTokenData = cs64_ini_token_data_alloc();
+
+    if(pTokenData == NULL) {
+        printf("Error empty_alloc_test: pTokenData failed to allocate! There is a very slight chance that the program ran out of memory.\n");
         return 2;
+    }
+
+    if(pTokenData->tokenAmount != 0) {
+        printf("Error empty_alloc_test: expected 0, but go pTokenData->tokenAmount %i.\n", pTokenData->tokenAmount);
+        return 3;
+    }
+
+    if(pTokenData->pLastPage != &pTokenData->firstPage) {
+        printf("Error empty_alloc_test: pTokenData->pLastPage %p != &pTokenData->firstPage %p.\n", pTokenData->pLastPage, &pTokenData->firstPage);
+        return 4;
+    }
+
+    if(pTokenData->firstPage.pNext != NULL) {
+        printf("Error empty_alloc_test: pTokenData->firstPage.pNext is supposed to be NULL not %p.\n", pTokenData->firstPage.pNext);
+        return 5;
+    }
+
+    cs64_ini_token_data_free(pTokenData);
+
+    if(pointerTrackAmount != 0) {
+        printf("Error empty_alloc_test: pointerTrackAmount is supposed to be zero after test not be %i.\n", pointerTrackAmount);
+        return 6;
     }
 
     return 0;
