2917310f1f49dab6761601a6288e0dc96b277db1
CoderScholar64 <126514127+CoderScholar64@users.noreply.github.com>
2024-12-20 11:58:13 -0800

    This tells me where the error is now.

--- src-tests/hash_table.c
+++ src-tests/hash_table.c
@@ -1157,7 +1157,7 @@ void cs64_ini_4_data_test() {
     UNIT_TEST_ASSERT_EQ(0, pSectionVarEntry[2]->entryType, CS64_INI_ENTRY_WAS_OCCUPIED, "This entry should have been removed with the section %d");
     UNIT_TEST_ASSERT_EQ(0, pSectionVarEntry[3]->entryType, CS64_INI_ENTRY_WAS_OCCUPIED, "This entry should have been removed with the section %d");
 
-    // Remove empty
+    // Remove empty section right.
     state = cs64_ini_del_entry(pData, pSectionEntry[3]);
     UNIT_TEST_ASSERT_EQ(0, state, CS64_INI_ENTRY_SUCCESS, "%d");
     UNIT_TEST_ASSERT(0, cs64_ini_get_variable(pData, NULL, cs64_ini_get_entry_name(pSectionEntry[3])) == NULL)
@@ -1166,6 +1166,7 @@ void cs64_ini_4_data_test() {
     UNIT_TEST_ASSERT(0, cs64_ini_get_next_entry(pSectionEntry[1]) == NULL);
     UNIT_TEST_ASSERT(0, pData->pFirstSection == cs64_ini_get_first_section(pData));
     UNIT_TEST_ASSERT(0, pData->pFirstSection == pSectionEntry[0]);
+    UNIT_TEST_ASSERT(0, pData->pLastSection  != pSectionEntry[3]);
     UNIT_TEST_ASSERT(0, pData->pLastSection  == pSectionEntry[1]);
 
     // Globals removal
