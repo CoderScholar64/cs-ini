e5fef949d69d79510e9337ecc94fe47979c03ac4
CoderScholar64 <126514127+CoderScholar64@users.noreply.github.com>
2025-01-01 15:23:41 -0800

    This fixes the memory leak issue

--- src/cs64_ini.h
+++ src/cs64_ini.h
@@ -2002,6 +2002,9 @@ CS64INIEntryState cs64_ini_set_entry_name(CS64INIData *pData, CS64INIEntry **ppE
                 }
             }
 
+            if(backupEntryType == CS64_INI_ENTRY_DYNAMIC_VALUE)
+                CS64_INI_FREE(pOldEntry->type.value.data.dynamic.pName);
+
             if(pDynamicMemory != NULL) {
                 pRenamedVariable->entryType = CS64_INI_ENTRY_DYNAMIC_VALUE;
                 pRenamedVariable->type.value.data.dynamic.pName = pDynamicMemory;
