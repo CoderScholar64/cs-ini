f60e09125f1537c04ba9f8c37141c2c8e45e4344
CoderScholar64 <126514127+CoderScholar64@users.noreply.github.com>
2025-01-26 19:20:15 -0800

    More unexpected error checks

--- src-tests/parse_line.c
+++ src-tests/parse_line.c
@@ -1122,6 +1122,49 @@ void cs64_ini_parser_expectation_test() {
         }
     }
 
+    {
+        /*
+         * Variable Tests.
+         * X = 5 Combo = 2 Total = 10
+         * [Comment END] Values Delem X{Delem, Comment, End, SecB, SecE}
+         */
+        CS64INITokenType types[] = {
+            CS64_INI_TOKEN_DELEMETER,
+            CS64_INI_TOKEN_COMMENT,
+            CS64_INI_TOKEN_END,
+            CS64_INI_TOKEN_SECTION_START,
+            CS64_INI_TOKEN_SECTION_END};
+
+        pToken[0]->type = CS64_INI_TOKEN_VALUE;
+        pToken[1]->type = CS64_INI_TOKEN_DELEMETER;
+        pToken[3]->type = CS64_INI_TOKEN_END;
+
+        index = 0;
+        while(index < sizeof(types) / sizeof(types[0])) {
+            UNEXPECTED_TOKEN_TEST(2);
+
+            UNIT_TEST_DETAIL_ASSERT(index, result.status.unexpected_token.expectedTokenAmount == 1, display_parser_result(&result););
+            UNIT_TEST_DETAIL_ASSERT(index, result.status.unexpected_token.pExpectedTokens[0] == CS64_INI_TOKEN_VALUE, display_parser_result(&result););
+
+            index++;
+        }
+
+        pToken[0]->type = CS64_INI_TOKEN_COMMENT;
+        pToken[1]->type = CS64_INI_TOKEN_END;
+        pToken[2]->type = CS64_INI_TOKEN_VALUE;
+        pToken[3]->type = CS64_INI_TOKEN_DELEMETER;
+        pToken[5]->type = CS64_INI_TOKEN_END;
+
+        index = 0;
+        while(index < sizeof(types) / sizeof(types[0])) {
+            UNEXPECTED_TOKEN_TEST(4);
+
+            UNIT_TEST_DETAIL_ASSERT(index, result.status.unexpected_token.expectedTokenAmount == 1, display_parser_result(&result););
+            UNIT_TEST_DETAIL_ASSERT(index, result.status.unexpected_token.pExpectedTokens[0] == CS64_INI_TOKEN_VALUE, display_parser_result(&result););
+
+            index++;
+        }
+    }
 
     cs64_ini_data_free(parserContext.pData);
     cs64_ini_lexer_free(parserContext.pTokenResult);
@@ -1133,16 +1176,10 @@ void cs64_ini_parser_expectation_test() {
      * { ... } Invalid Token Options
      *
      * Variable Tests.
-     * X = 4 Combo = 2 Total =  8
-     * [Comment END] Values X{Comment, End, SecB, SecE}
+     * X = 3 Combo = 2 Total =  6
+     * [Comment END] Values Delem Values X{Delem, SecB, SecE}
      * X = 5 Combo = 2 Total = 10
-     * [Comment END] Values Delem X{Delem, Comment, End, SecB, SecE}
-     * X = 3 Combo = 2 Total =  6
-     * [Comment END] Values Delem Values X{Delem, SecB, SecE}
-     * X = 3 Combo = 2 Total =  6
-     * [Comment END] Values Delem Values X{Delem, SecB, SecE}
-     * X = 4 Combo = 2 Total =  8
-     * [Comment END] Values Delem Values X{Delem, Value, SecB, SecE}
+     * [Comment END] Values Delem Values Comment X{Delem, Comment, Value, SecB, SecE}
      */
 }
 
